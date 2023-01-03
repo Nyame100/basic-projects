b7e736a085010d529863bec2412d6fb19f7fa4eb
Nyame100 <nyamerichmond100@gmail.com>
2023-01-03 17:05:25 +0100

    Accordion project

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
-}
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {data.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
+}
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ id, title, info }) => {
+  const [answer, setAnswer] = useState(false);
+  return (
+    <section className="question">
+      <div>
+        <header>
+          <h4>{title}</h4>
+          <button
+            className="btn"
+            onClick={() => {
+              return setAnswer(!answer);
+            }}
+          >
+            {answer ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          </button>
+        </header>
+        {/* {answer && <p>{info}</p>} */}
+        <p>{answer ? info : `${info.substring(0, 0)}`}</p>
+      </div>
+    </section>
+  );
 };
 
 export default Question;
